cf35c3f907d9645af072b7a72ee600750435b8f7
polo <lg550055@hotmail.com>
2022-05-23 18:12:52 -0500

    Adds urls, views and templates

---
+++ snacks/urls.py
@@ -0,0 +1,9 @@
+from unicodedata import name
+from django.urls import path
+
+from .views import HomeView, AboutView
+
+urlpatterns = [
+  path('', HomeView.as_view(), name='home'),
+  path('about/', AboutView.as_view(), name='about')
+]
